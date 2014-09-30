82f4c078c6d68954bbfda8af253ee9b67d147fab
echoe <sfelshman@gmail.com>
2014-09-29 23:21:14 -0500

    Added variable-editing first section
    Change values by changing the numbers!

--- ssinter.sh
+++ ssinter.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #sys-snap interpreter, written for cPanel's version
 #https://github.com/echoe/sys-snap-interpreter/
-#Version 0.14
+#Version 0.15
+loadbound=5
+membound=50000
+swapbound=5000
+waitbound=10
+cpubound=100
+networkbound=100
+mysqlbound=20
+apachebound=100
 
 function checkload
 {
@@ -10,7 +18,7 @@ function checkload
         load1min=`echo $top | awk '{print $6}' |cut -d. -f1`
         load5min=`echo $top | awk '{print $7}' |cut -d. -f1`
         load10min=`echo $top | awk '{print $8}' |cut -d. -f1`
-        if [ $load1min -gt 5 ] || [ $load5min -gt 5 ] || [ $load10min -gt 5 ]; then
+        if [ $load1min -gt $loadbound ] || [ $load5min -gt $loadbound ] || [ $load10min -gt $loadbound ]; then
                 echo $top;
         fi
 }
@@ -20,7 +28,7 @@ function checkmem
         memory=`sed -n '/Memory Usage:/,/Virtual Memory Stats:/p' $1 | head -n -2 | tail -n +3`
         memfree=`echo $memory | grep MemFree | awk '{print $2}'`
         swapfree=`echo $memory | grep SwapFree | awk '{print $2}'`
-        if [ $memfree -lt 50000 ] || [ $swapfree -lt 5000 ] ; then 
+        if [ $memfree -lt $membound ] || [ $swapfree -lt $swapbound ] ; then 
                 echo $thetime "Free Memory" $memfree "Swap" $swapfree; 
         fi
 }
@@ -31,7 +39,7 @@ function checkvirtmem
         waittime=`echo $virtualmemory | rev | awk '{print $2}' | rev`
         waitissues="n"; 
         for i in $waittime; do 
-                if [ $i > 10 ] ; then 
+                if [ $i > $waitbound ] ; then 
                         waitissues="y"; 
                 fi; 
         done
@@ -45,8 +53,8 @@ function checkps
         processcpu=`sed -n '/Process List:/,/Network Connections:/p' $1 | head -n -2 | tail -n +4 | sort -nrk 3 | head -n2`
         echo $processcpu | while read ; do
                 line=$REPLY ;
-                if [ `echo $line | awk '{print $3}' | cut -d. -f1` -gt 100 ] ; then
-                        echo "Time" $thetime "Cpu over 100";
+                if [ `echo $line | awk '{print $3}' | cut -d. -f1` -gt $cpubound ] ; then
+                        echo "Time" $thetime "Cpu over " $cpubound;
                 fi;
         done;
 }
@@ -56,7 +64,7 @@ function checknetwork
         networklist=`sed -n '/Active Internet connections/,/Active UNIX domain sockets/p' $1 | head -n -1 | tail -n +3`
         networknum=`echo $networklist | wc -l`
         networkips=`echo $networklist | awk '{print $5}' | sort | uniq -c | head -n 5`
-        if [[ $networknum -gt 100 ]] ; then
+        if [[ $networknum -gt $networkbound ]] ; then
                 echo "Time" $thetime "Number of httpd connections" $networknum "exploiting IPs" $networkips;
         fi
 }
@@ -65,7 +73,7 @@ function checkmysql
 {
         mysqllist=`sed -n '/MYSQL Processes:/,/Apache Processes:/p' $1 | head -n -2 | tail -n +3`
         mysqlnum=`echo $mysqllist | wc -l`
-        if [[ $mysqlnum -gt 20 ]] ; then
+        if [[ $mysqlnum -gt $mysqlbound ]] ; then
                 echo "Date" $thetime "Number of mysql connections" $mysqlnum;
         fi
 }
@@ -74,7 +82,7 @@ function checkapache
 {
         apachelist=`sed -n '/Apache Processes:/,/Network Connections:/p' $1 | head -n -2 | tail -n +3`
         apachenum=`echo $apachelist | wc -l`
-        if [[ $apachenum -gt 100 ]] ; then
+        if [[ $apachenum -gt $apachebound ]] ; then
                 echo "Date" $thetime "Number of httpd connections" $apachenum;
         fi
 }
