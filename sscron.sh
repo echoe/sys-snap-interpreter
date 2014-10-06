feb92fe9315159a7667f91629fea0424eb91e553
echoe <sfelshman@gmail.com>
2014-10-06 15:27:54 -0500

    update
    just making it cleaner and use more accepted bash commands.

--- sscron.sh
+++ sscron.sh
@@ -1,15 +1,14 @@
-#This installs a sys-snap cronjob and runs recordkeeping to keep sys-snap [and this script!] up to date.
-#To run this: bash <(curl https://raw.githubusercontent.com/echoe/sys-snap-interpreter/master/sscron.sh)
-#Version 0.01
+#This installs a sys-snap cronjob and does maintenance on the 'install'.
+#Version 0.10
+if [[ ! -e /root/sys-snap.pl ]]; then
+        wget -N -P /root/ https://raw.githubusercontent.com/echoe/sys-snap-interpreter/master/sys-snap.pl
+fi
+if [[ ! -e /root/sscron.sh ]]; then
+        wget -N -P /root/ https://raw.githubusercontent.com/echoe/sys-snap-interpreter/master/sscron.sh
+fi
+if [[ ! -e /etc/cron.d/syssnap ]]; then
+    echo "0 0 * * * /bin/sh /usr/local/bin/sscron.sh" > /etc/cron.d/syssnap
+fi
 ps aux | awk '/[s]ys-snap/ {print$2}' | xargs kill
-if [[ -z `/bin/ls -A /root/sys-snap.pl` ]] ; then
-        su - -c "wget -N -P /root https://raw.githubusercontent.com/cPanelTechs/SysSnap/master/sys-snap.pl"
-fi
 perl /root/sys-snap.pl 2>/dev/null &
-if [[ -z `crontab -l | grep "sscron.sh"` ]]; then
-    crontab -l > /tmp/mycrontab.txt;
-    echo "0 0 * * * /bin/sh /root/sscron.sh" >> /tmp/mycrontab.txt;
-    crontab /tmp/mycrontab.txt;
-    rm -f /tmp/mycrontab.txt
-fi
-find /root/system-snapshot* -mtime +14 -delete
+find /root/system-snapshot* -type d -mtime +14 -delete
